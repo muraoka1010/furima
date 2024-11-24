7c02d2f1480ec8ba1f5d719e8ed6003169594fcf
muraoka1010 <yuto80285@gmail.com>
2024-11-24 20:41:05 +0900

    修正

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -1,12 +1,18 @@
 const price = () => {
   const priceInput = document.getElementById("item-price");
   const addTaxDom = document.getElementById("add-tax-price");
+  const profitDom = document.getElementById("profit");
+  
   priceInput.addEventListener("input", () => {
     const inputValue = priceInput.value;
     console.log(inputValue);
 
    const tax = Math.floor(inputValue * 0.1); 
     addTaxDom.innerHTML = tax;
+
+    const profit = inputValue - tax;
+    profitDom.innerHTML = profit;
+
   })
  
 };
