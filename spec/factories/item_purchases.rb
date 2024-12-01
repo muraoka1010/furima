7fd51ddc3953815f98c1557ca656b8a30f17d664
muraoka1010 <yuto80285@gmail.com>
2024-12-01 16:18:41 +0900

    テストコード実装

---
+++ spec/factories/item_purchases.rb
@@ -0,0 +1,13 @@
+FactoryBot.define do
+  factory :item_purchase do
+    user_id { 1 }
+    item_id { 1 }
+    postal_code { '123-4567' }
+    prefecture_id { 2 }
+    city { '東京都' }
+    address { '渋谷区1-1' }
+    building_name { 'テストビル' }
+    phone_number { '09012345678' }
+    token { 'tok_abcdefghijk00000000000000000' }
+  end
+end
