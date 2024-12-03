5487f96d5f10ffa08de957e867d49423b349db24
muraoka1010 <yuto80285@gmail.com>
2024-12-03 18:09:16 +0900

    環境変数の設定

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,20 +1,20 @@
 class PurchasesController < ApplicationController
-
+  before_action :authenticate_user!, only: [:index, :create]
   def index
+    gon.public_key = ENV["PAYJP_PUBLIC_KEY"]
     @item = Item.find(params[:item_id])
+    if @item.purchase.present? ||  current_user == @item.user
+      redirect_to root_path
+    end
     @item_purchase = ItemPurchase.new
   end
 
   def create
+    gon.public_key = ENV["PAYJP_PUBLIC_KEY"]
     @item_purchase = ItemPurchase.new(purchase_params)
     @item = Item.find(params[:item_id])
     if @item_purchase.valid?
-      Payjp.api_key = "sk_test_4da2cfb18252871f0355de9b"  # 自身のPAY.JPテスト秘密鍵を記述しましょう
-      Payjp::Charge.create(
-        amount: @item.price,  # 商品の値段
-        card: purchase_params[:token],    # カードトークン
-        currency: 'jpy'                 # 通貨の種類（日本円）
-      )
+      pay_item
       @item_purchase.save
       redirect_to root_path
     else
@@ -29,6 +29,15 @@ class PurchasesController < ApplicationController
     params.require(:item_purchase).permit(
     :postal_code, :prefecture_id, :city, :address, :building_name, :phone_number, :token).
     merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
+  end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      amount: @item.price,  # 商品の値段
+      card: purchase_params[:token],    # カードトークン
+      currency: 'jpy'                 # 通貨の種類（日本円）
+    )
   end
 
 end
--- app/javascript/card.js
+++ app/javascript/card.js
@@ -1,5 +1,6 @@
 const pay = () => {
-  const payjp = Payjp("pk_test_89d49b5a6dc54d295ffc4a0e");
+  const publicKey = gon.public_key
+  const payjp = Payjp(publicKey)
   const elements = payjp.elements();
   const numberElement = elements.create('cardNumber');
   const expiryElement = elements.create('cardExpiry');
@@ -28,3 +29,4 @@ const pay = () => {
 };
 
 window.addEventListener("turbo:load", pay);
+window.addEventListener("turbo:render", pay);
